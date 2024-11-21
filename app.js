ac2f070ddfe5ec48e985a6a46b119c1542112a0f
Michal <31184485+MichalKuracina@users.noreply.github.com>
2024-11-21 19:29:49 +0100

    animation added

--- app.js
+++ app.js
@@ -46,6 +46,39 @@ function run() {
     coordinates(app, canvasWidth, canvasHeight);
     hudContainer = hud();
 
+    const atlasData = {
+      frames: {
+        enemy1: {
+          frame: { x: 0, y: 0, w: 64, h: 64 },
+          sourceSize: { w: 64, h: 64 },
+          spriteSourceSize: { x: 0, y: 0, w: 64, h: 64 },
+        },
+        enemy2: {
+          frame: { x: 64, y: 0, w: 64, h: 64 },
+          sourceSize: { w: 64, h: 64 },
+          spriteSourceSize: { x: 0, y: 0, w: 64, h: 64 },
+        },
+      },
+      meta: {
+        image: "/assets/roads2W.png",
+        // format: 'RGBA8888',
+        size: { w: 512, h: 192 },
+        // scale: 1
+      },
+      animations: {
+        enemy: ["enemy1", "enemy2"], //array of frames by name
+      },
+    };
+
+    const texture = await PIXI.Assets.load(atlasData.meta.image);
+    const spritesheet = new PIXI.Spritesheet(texture, atlasData);
+    await spritesheet.parse();
+    const animatedSprite = new PIXI.AnimatedSprite(
+      spritesheet.animations.enemy
+    );
+    app.stage.addChild(animatedSprite);
+    animatedSprite.play();
+    animatedSprite.animationSpeed = 0.15;
     // app.stage.on("pointerdown", (event) => {
     //   const mousePosition = event.data.global;
     //   enemy = new Enemy(
